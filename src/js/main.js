1e3a9e4262af7097c2056f62900ddec1e5c4749b
Test Name <majiroikione@gmail.com>
2025-08-19 13:29:44 +0300

    Added testimonials section

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,11 @@
 import '../assets/scss/main.scss'
 
 // components
-import {useTheme} from './components/theme';
-import {useBurger} from './components/burger';
-import {useInsightSlider} from './components/slider';
+import {useTheme} from './components/theme.js';
+import {useBurger} from './components/burger.js';
+import {useInsightSlider, useTestimonialsSlider} from './components/home/slider.js';
 
 useTheme();
 useBurger();
 useInsightSlider();
+useTestimonialsSlider();
